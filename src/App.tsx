11dcd0d252c11bba3cb5691029798e8803ca404b
dasha7879 <belka7879@gmail.com>
2023-01-04 14:31:52 +0300

    feat: add NewCopmonent

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,32 @@ import './App.css';
 import { Header } from './Header';
 import { Body } from './Body';
 import { Footer } from './Footer';
+import { NewComponent } from './NewComponent';
 
 function App() {
+
+  // <>
+  //   <Header title='New header' />
+  //   <Body titleForBody='New  Body' />
+  //   <Footer titleForFooter='New Footer' />
+  //   </>
+  const students = [
+    {id: 1, name: "James", age: 8},
+    {id: 2, name: "Robert", age: 18},
+    {id: 3, name: "John", age: 28},
+    {id: 4, name: "Michael", age: 38},
+    {id: 5, name: "William", age: 48},
+    {id: 6, name: "David", age: 58},
+    {id: 7, name: "Richard", age: 68},
+    {id: 8, name: "Joseph", age: 78},
+    {id: 9, name: "Thomas", age: 88},
+    {id: 10, name: "Charles", age: 98},
+    {id: 11, name: "Christopher", age: 100},
+]
+
   return (
-    <>
-      <Header title='New header' />
-      <Body titleForBody='New  Body' />
-      <Footer titleForFooter='New Footer' />
-      </>
-      );
+    <NewComponent students={students}/>
+  );
 }
 
-      export default App;
+export default App;
