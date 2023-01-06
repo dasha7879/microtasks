3a09572e5421b0f8c55ccc24861b5dd71feecf68
dasha7879 <belka7879@gmail.com>
2023-01-06 13:58:47 +0300

    feat: add solve for filter-task

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Header } from './Header';
-import { Body } from './Body';
-import { Footer } from './Footer';
+import { link } from 'fs';
+// import { NewComponent } from './NewComponent';
+import { isPropertySignature } from 'typescript';
+import { Button } from './Button';
 import { NewComponent } from './NewComponent';
-import { Cars } from './Cars';
+
+
+type FilterType = 'all' | 'Dollars' | 'RUBLS'
 
 function App() {
 
-  // <>
-  //   <Header title='New header' />
-  //   <Body titleForBody='New  Body' />
-  //   <Footer titleForFooter='New Footer' />
-  //   </>
-  const students = [
-    {id: 1, name: "James", age: 8},
-    {id: 2, name: "Robert", age: 18},
-    {id: 3, name: "John", age: 28},
-    {id: 4, name: "Michael", age: 38},
-    {id: 5, name: "William", age: 48},
-    {id: 6, name: "David", age: 58},
-    {id: 7, name: "Richard", age: 68},
-    {id: 8, name: "Joseph", age: 78},
-    {id: 9, name: "Thomas", age: 88},
-    {id: 10, name: "Charles", age: 98},
-    {id: 11, name: "Christopher", age: 100},
-]
+  const [money, setMoney] = useState([
+    { banknots: 'Dollars', value: 100, number: ' a1234567890' },
+    { banknots: 'Dollars', value: 50, number: ' z1234567890' },
+    { banknots: 'RUBLS', value: 100, number: ' w1234567890' },
+    { banknots: 'Dollars', value: 100, number: ' e1234567890' },
+    { banknots: 'Dollars', value: 50, number: ' c1234567890' },
+    { banknots: 'RUBLS', value: 100, number: ' r1234567890' },
+    { banknots: 'Dollars', value: 50, number: ' x1234567890' },
+    { banknots: 'RUBLS', value: 50, number: ' v1234567890' },
+  ])
 
-const topCars = [
-  {manufacturer:'BMW', model:'m5cs'},
-  {manufacturer:'Mercedes', model:'e63s'},
-  {manufacturer:'Audi', model:'rs6'}
-]
+
+
+  //локальный стейт, который записывает значение кнопки
+  const [filter, setFilter] = useState<FilterType>('all')
+
+
+
+
+  const onClickFilterHandler = (nameButton: FilterType) => {
+    setFilter(nameButton);
+  }
+
+
+
+
 
   return (
     <>
-    <NewComponent students={students}/>
-    <Cars cars = {topCars}/>
+      <Button name='all' callBack={() => (onClickFilterHandler('all'))} />
+      <Button name='RUBLS' callBack={() => (onClickFilterHandler('RUBLS'))} />
+      <Button name='Dollars' callBack={() => (onClickFilterHandler('Dollars'))} />
+      <NewComponent money={money} filter={filter} />
     </>
-       
-  );
+
+  )
 }
 
 export default App;
---
+++ src/Button.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+import { useCallback } from "react"
+import { isPropertySignature } from "typescript"
+
+type ButtonType ={
+    name: string
+    callBack: ()=> void
+
+}
+
+
+export const Button = (props: ButtonType) => {
+
+const onClickHandler = () => {
+    props.callBack()
+}
+
+return (
+    <>
+    <button onClick={ onClickHandler}>{props.name}</button>
+    </>
+
+)}
